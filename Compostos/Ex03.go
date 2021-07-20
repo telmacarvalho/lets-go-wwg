dceba74a4caf6dfccb58638a6d377ad1d0190195
Telma Carvalho <telmaclarita@gmail.com>
2021-07-19 22:37:14 -0300

    Alteraçao do exercicio usando range.

--- Compostos/Ex03.go
+++ Compostos/Ex03.go
@@ -1,3 +1,8 @@
+// Escreva um programa que lista o nome dos países e quantas vezes eles aparecem no nosso map.
+// 1) Criar um mapa com chave do tipo string e valor do tipo string (map[string]string) onde a chave seja o nome da cidade e o valor o nome do país.
+// 2) Percorrer o mapa do item 1 acumulando em outro mapa a frequência de aparições do país. Esse mapa segundo mapa terá tipo map[string]int, sendo a chave o nome do país e o valor a quantidade de menções.
+// 3) Printe na tela os valores do último mapa.
+
 package main
 
 import "fmt"
@@ -13,139 +18,23 @@ func main() {
 	cidades["Toronto"] = "Canadá"
 	cidades["Barra Mansa"] = "Brasil"
 	fmt.Println(cidades)
-	fmt.Println(cidades["Rio de Janeiro"])
 
 	contagem := make(map[string]int)
-	contagem["Brasil"] = 0
-	contagem["França"] = 0
-	contagem["Inglaterra"] = 0
-	contagem["EUA"] = 0
-	contagem["Canadá"] = 0
 
-	if cidades["Rio de Janeiro"] == "Brasil" {
-		contagem["Brasil"] = contagem["Brasil"] + 1
-	}
-	if cidades["Rio de Janeiro"] == "França" {
-		contagem["França"] = contagem["França"] + 1
-	}
-	if cidades["Rio de Janeiro"] == "Inglaterra" {
-		contagem["Inglaterra"] = contagem["Inglaterra"] + 1
-	}
-	if cidades["Rio de Janeiro"] == "EUA" {
-		contagem["EUA"] = contagem["EUA"] + 1
-	}
-	if cidades["Rio de Janeiro"] == "Canadá" {
-		contagem["Canadá"] = contagem["Canadá"] + 1
-	}
-
-	if cidades["Paris"] == "Brasil" {
-		contagem["Brasil"] = contagem["Brasil"] + 1
-	}
-	if cidades["Paris"] == "França" {
-		contagem["França"] = contagem["França"] + 1
-	}
-	if cidades["Paris"] == "Inglaterra" {
-		contagem["Inglaterra"] = contagem["Inglaterra"] + 1
-	}
-	if cidades["Paris"] == "EUA" {
-		contagem["EUA"] = contagem["EUA"] + 1
-	}
-	if cidades["Paris"] == "Canadá" {
-		contagem["Canadá"] = contagem["Canadá"] + 1
-	}
-	if cidades["Londres"] == "Brasil" {
-		contagem["Brasil"] = contagem["Brasil"] + 1
-	}
-	if cidades["Londres"] == "França" {
-		contagem["França"] = contagem["França"] + 1
-	}
-	if cidades["Londres"] == "Inglaterra" {
-		contagem["Inglaterra"] = contagem["Inglaterra"] + 1
-	}
-	if cidades["Londres"] == "EUA" {
-		contagem["EUA"] = contagem["EUA"] + 1
-	}
-	if cidades["Londres"] == "Canadá" {
-		contagem["Canadá"] = contagem["Canadá"] + 1
-	}
-	if cidades["São Paulo"] == "Brasil" {
-		contagem["Brasil"] = contagem["Brasil"] + 1
-	}
-	if cidades["São Paulo"] == "França" {
-		contagem["França"] = contagem["França"] + 1
-	}
-	if cidades["São Paulo"] == "Inglaterra" {
-		contagem["Inglaterra"] = contagem["Inglaterra"] + 1
-	}
-	if cidades["São Paulo"] == "EUA" {
-		contagem["EUA"] = contagem["EUA"] + 1
-	}
-	if cidades["São Paulo"] == "Canadá" {
-		contagem["Canadá"] = contagem["Canadá"] + 1
-	}
-
-	if cidades["Chicago"] == "Brasil" {
-		contagem["Brasil"] = contagem["Brasil"] + 1
-	}
-	if cidades["Chicago"] == "França" {
-		contagem["França"] = contagem["França"] + 1
-	}
-	if cidades["Chicago"] == "Inglaterra" {
-		contagem["Inglaterra"] = contagem["Inglaterra"] + 1
-	}
-	if cidades["Chicago"] == "EUA" {
-		contagem["EUA"] = contagem["EUA"] + 1
-	}
-	if cidades["Chicago"] == "Canadá" {
-		contagem["Canadá"] = contagem["Canadá"] + 1
-	}
-
-	if cidades["Nova York"] == "Brasil" {
-		contagem["Brasil"] = contagem["Brasil"] + 1
-	}
-	if cidades["Nova York"] == "França" {
-		contagem["França"] = contagem["França"] + 1
-	}
-	if cidades["Nova York"] == "Inglaterra" {
-		contagem["Inglaterra"] = contagem["Inglaterra"] + 1
-	}
-	if cidades["Nova York"] == "EUA" {
-		contagem["EUA"] = contagem["EUA"] + 1
-	}
-	if cidades["Nova York"] == "Canadá" {
-		contagem["Canadá"] = contagem["Canadá"] + 1
-	}
-
-	if cidades["Toronto"] == "Brasil" {
-		contagem["Brasil"] = contagem["Brasil"] + 1
-	}
-	if cidades["Toronto"] == "França" {
-		contagem["França"] = contagem["França"] + 1
-	}
-	if cidades["Toronto"] == "Inglaterra" {
-		contagem["Inglaterra"] = contagem["Inglaterra"] + 1
-	}
-	if cidades["Toronto"] == "EUA" {
-		contagem["EUA"] = contagem["EUA"] + 1
-	}
-	if cidades["Toronto"] == "Canadá" {
-		contagem["Canadá"] = contagem["Canadá"] + 1
-	}
-
-	if cidades["Barra Mansa"] == "Brasil" {
-		contagem["Brasil"] = contagem["Brasil"] + 1
-	}
-	if cidades["Barra Mansa"] == "França" {
-		contagem["França"] = contagem["França"] + 1
-	}
-	if cidades["Barra Mansa"] == "Inglaterra" {
-		contagem["Inglaterra"] = contagem["Inglaterra"] + 1
-	}
-	if cidades["Barra Mansa"] == "EUA" {
-		contagem["EUA"] = contagem["EUA"] + 1
-	}
-	if cidades["Barra Mansa"] == "Canadá" {
-		contagem["Canadá"] = contagem["Canadá"] + 1
+	for i, v := range cidades {
+		if v == "Brasil" {
+			contagem["Brasil"] = contagem["Brasil"] + 1
+		} else if v == "França" {
+			contagem["França"] = contagem["França"] + 1
+		} else if v == "Inglaterra" {
+			contagem["Inglaterra"] = contagem["Inglaterra"] + 1
+		} else if v == "EUA" {
+			contagem["EUA"] = contagem["EUA"] + 1
+		} else if v == "Canadá" {
+			contagem["Canadá"] = contagem["Canadá"] + 1
+		} else {
+			fmt.Println(i)
+		}
 	}
 
 	fmt.Println(contagem)
